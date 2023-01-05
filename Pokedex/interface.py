87b6bb47ecbe12cbeee893c55f28e71e5fee7d20
Dimitri Matheus de Oliveira <dimitrimatheusdeoliveira@gmail.com>
2023-01-04 21:14:08 -0400

    Alteração no interface
    Criando os textos.

--- Pokedex/interface.py
+++ Pokedex/interface.py
@@ -1,6 +1,7 @@
 # Modulos
 from tkinter import *
 from tkinter import ttk
+from PIL import Image, ImageTk
 from ttkthemes import ThemedTk
 
 # cores
@@ -24,5 +25,16 @@ ttk.Separator(janela, orient="horizontal").grid(row=0, columnspan=1, ipadx=272)
 frame_pokemon = Frame(janela, width=550, height=230, relie='flat')
 frame_pokemon.grid(row=1, column=0)
 
+# Nome
+poke_nome = Label(frame_pokemon, text='Pikachu', relief='flat', anchor=CENTER, font=('Fixedsys 20'), bg=Branco, fg=Preto)
+poke_nome.place(x=12, y=15)
+
+# Tipo
+poke_tipo = Label(frame_pokemon, text='Elétrico', relief='flat', anchor=CENTER, font=('Ivy 10 bold'), bg=Branco, fg=Preto)
+poke_tipo.place(x=12, y=50)
+
+# ID
+poke_id = Label(frame_pokemon, text='#025', relief='flat', anchor=CENTER, font=('Ivy 10 bold'), bg=Branco, fg=Preto)
+poke_id.place(x=12, y=75)
 
 janela.mainloop()
